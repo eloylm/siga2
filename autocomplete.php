6492f9415c57319873c9f697bd7852befed1642d
Leandro <lmoreno@institutoquimicoarg.com.ar>
2021-06-29 12:07:02 -0300

    prueba

--- autocomplete.php
+++ autocomplete.php
@@ -1,37 +1,37 @@
 <?php
 	require('includes/prepend.inc.php');
-
+#prueba
 	class SampleForm extends QForm {
 		protected $txtServerSide;
 		protected $txtClientSide;
 
                 protected $lblCliente;
                 //protected $lstCliente;
-               
+
 
 		protected function Form_Create() {
-		
+
                         $this->lblCliente = new QLabel($this);
                         $this->lblCliente->Name = 'Cliente';
                         $this->lblCliente->Text = 'Nada';
                          $this->txtClientSide = new QAutoCompleteTextBox($this);
-                        
+
                         $clientes = Clientes::LoadAll();
 
                         foreach ($clientes as $cliente){
                             $lstCliente = new QListItem($cliente->Nombre,$cliente->ClientesId);
                             $this->txtClientSide->AddItem($lstCliente);
                             $arrAutoCompleteItems[] =$cliente->Nombre." ".$cliente->ClientesId;
-                            
+
                         }
-                      
+
                        $this->txtClientSide->Name = QApplication::Translate('Keyword');
                        $this->txtClientSide->AddAction(new QChangeEvent(), new QAjaxAction('txtCliente_Change'));
                         print_r($this->txtClientSide);
-                        
+
                 }
               protected function txtCliente_Change($strFormId, $strControlId, $strParameter){
-                 
+
                   foreach ($this->txtClientSide->SelectedValue as $value){
                       $this->lblCliente->Text = $value;
                   }
